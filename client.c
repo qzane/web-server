5038dcaf567a663bce712a67e8ca1d039b2a56d6
unknown <qzane@live.com>
2015-10-06 16:34:22 +0800

    use inet_addr

--- client.c
+++ client.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
-    struct hostent *server;
+    //struct hostent *server;
 
     char buffer[256];
     if (argc < 3) {
@@ -28,17 +28,18 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+    //server = gethostbyname(argv[1]);
+    //if (server == NULL) {
+    //    fprintf(stderr,"ERROR, no such host\n");
+    //    exit(0);
+    //}
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
     serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);  
+    //bcopy((char *)server->h_addr, 
+    //     (char *)&serv_addr.sin_addr.s_addr,
+    //    server->h_length);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
     printf("Please enter the message: ");
